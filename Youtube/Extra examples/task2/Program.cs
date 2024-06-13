7180d91f00732b6037f5bef02fd05aeccefbf72d
WRAV101 <schoolmosia@gmail.com>
2024-06-13 20:48:25 +0200

    added task2

---
+++ Youtube/Extra examples/task2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace task2
+{
+    class Program
+    {
+        enum Month
+        {
+            JANUARY=1, FEBRUARY, MARCH, APRIL, MAY, JUNE,
+            JULY, AUGUST, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a month number: ");
+            int monthNumber = int.Parse(Console.ReadLine());
+
+            Month monthName = (Month)monthNumber;
+
+            Console.WriteLine("{0} is month number {1}", monthName, monthNumber);
+        }
+    }
+}
